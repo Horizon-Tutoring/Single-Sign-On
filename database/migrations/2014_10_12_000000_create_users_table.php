b84de595db5a31d3ce5022a0e6b94e92a2172ac5
JoshuaMicallefYBSU <joshuamicallef66@gmail.com>
2023-01-05 12:09:53 +1000

    SSO Page Changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id()->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+			$table->string('username')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+			$table->string('password');
+            $table->string('account_type')->nullable();
+            $table->string('staff_type')->nullable();
             $table->timestamps();
         });
     }
